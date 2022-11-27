cb4a7b501811641f0fd0befb0ccde0cbc4e72b49
Marcin Dłubakowski <marcin@dlubakowski.pl>
2022-11-27 11:03:12 +0100

    Update

--- zadanie_1.py
+++ zadanie_1.py
@@ -1,3 +1,13 @@
+import json
 from rev_index import build_rev_index, read_all_documents
 
-print(build_rev_index(read_all_documents()))
+def __index_objects_to_map__(object_list):
+    object_dict = {}
+    for obj in object_list: object_dict[obj.doc_id] = obj.doc_occurences
+    return object_dict
+
+reverse_index = build_rev_index(read_all_documents())
+reverse_index = dict((k, __index_objects_to_map__(v)) for k, v in reverse_index.items())
+
+with open("reverse_index_of_cars.txt", "w+", encoding="utf-8") as file:
+    file.write(json.dumps(reverse_index, indent=4))
